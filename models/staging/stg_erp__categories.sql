0bcdc2ddd9ca73401ec876f294d8a2a49c6323c7
Cleber Guimarães <cleber_guimaraes@hotmail.com>
2023-09-29 14:52:49 +0000

    Padronização dos campos

--- models/staging/stg_erp__categories.sql
+++ models/staging/stg_erp__categories.sql
@@ -1,6 +1,10 @@
 with
     fonte_categories as (
-        select *
+        select 
+            cast(category_id as int) as id_categoria
+            , cast(category_name as string) as nome_categoria
+            , cast(description as string) as descricao_categoria
+            --, cast(picture as string) foto
         from {{ source('erp', 'categories') }}
     )
 
