2fac5d62173d12475dcaebef39118f50854381c4
Cleber Guimarães <cleber_guimaraes@hotmail.com>
2023-09-30 13:25:07 +0000

    Criei stgs produtos e fornecedores.

--- models/staging/stg_erp__categorias.sql
+++ models/staging/stg_erp__categorias.sql
@@ -1,5 +1,5 @@
 with
-    fonte_categories as (
+    fonte_categorias as (
         select 
             cast(category_id as int) as id_categoria
             , cast(category_name as string) as nome_categoria
@@ -8,4 +8,4 @@ with
         from {{ source('erp', 'categories') }}
     )
 
-select * from fonte_categories   
+select * from fonte_categorias   
